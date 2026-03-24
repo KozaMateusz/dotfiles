3cf2accb6584738155c10f9416f0b484b0ebc35d
Mateusz Koza <mateusz.koza@grinn-global.com>
2026-03-24 13:12:47 +0100

    nvim: add next/prev buffer keybind

---
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,7 @@
+return {
+  "akinsho/bufferline.nvim",
+  keys = {
+    { "<leader>bn", "<Cmd>BufferLineCycleNext<CR>", desc = "Next Buffer" },
+    { "<leader>bb", "<Cmd>BufferLineCyclePrev<CR>", desc = "Previous Buffer" },
+  },
+}
