d57aa107af020449fb24af82289aff954e971642
Mateusz Koza <mateusz.koza@grinn-global.com>
2026-03-25 18:25:41 +0100

    waybar: add capslock icon

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,12 +3,14 @@
 -- Add any additional options here
 vim.opt.relativenumber = true
 vim.opt.colorcolumn = "80"
-vim.opt.expandtab = false
-vim.opt.shiftwidth = 8
-vim.opt.tabstop = 8
-vim.opt.softtabstop = 8
+-- vim.opt.expandtab = false
+-- vim.opt.shiftwidth = 8
+-- vim.opt.tabstop = 8
+-- vim.opt.softtabstop = 8
 vim.opt.smartindent = true
 vim.opt.cindent = true
 vim.opt.cinoptions = "L0,(0,W4"
 vim.opt.list = true
 vim.opt.listchars = { tab = "▸ " }
+
+vim.g.autoformat = false
