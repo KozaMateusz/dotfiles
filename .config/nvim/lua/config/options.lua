72f35b81a59a330d2413dceb1925ddcd968fb4e5
Mateusz Koza <mateusz.koza@grinn-global.com>
2026-04-15 15:48:57 +0200

    nvim: colorcolumn at 72

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = true
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "72"
 -- vim.opt.expandtab = false
 -- vim.opt.shiftwidth = 8
 -- vim.opt.tabstop = 8
